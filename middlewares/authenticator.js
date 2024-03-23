c594b281f476a5091c7aa9a2a0ac1ac2a229a3b7
Vivek <www.vivekeduchigure@gmail.com>
2024-03-23 20:58:38 +0530

    first commit

---
+++ middlewares/authenticator.js
@@ -0,0 +1,23 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+function authenticator(req, res, next) {
+  const token = req.headers.authorization;
+  jwt.verify(token, "vivek", (err, decode) => {
+    if (err)
+      return res.send({
+        message: "Token is not valid",
+        status: 2,
+      });
+    if (decode) {
+      req.body.user = decode.userId;
+      next();
+    } else {
+      res.send({
+        message: "Token is not valid",
+        status: 2,
+      });
+    }
+  });
+}
+
+module.exports = { authenticator };
